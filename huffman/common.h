5c22409375e49df86af07ae76a928a17581ff9d5
bord81 <bslabut@gmail.com>
2018-06-24 13:52:38 +0300

    Create common.h

---
+++ huffman/common.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <setjmp.h>
+
+enum orig_fsize_bb {ONE = 1, TWO = 2, FOUR = 4, EIGHT = 8};
+typedef enum orig_fsize_bb o_fsize;
